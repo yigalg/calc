da564a3956ea48c3bf75f37fdc09f2e2242afa7d
yigalg <github@gvili.net>
2016-09-14 10:23:01 +0000

    version 2

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -21,7 +21,7 @@ public class Calc {
             String inputFile = null;
             if ( args.length>0 ) inputFile = args[0];
             else {
-                System.out.println("Type the calculator expressions and press enter after each expression, press ctrl+D when done.");
+                System.out.println("Version 2. Type the calculator expressions and press enter after each expression, press ctrl+D when done.");
             }
             InputStream is = System.in;
             if ( inputFile!=null ) is = new FileInputStream(inputFile);
